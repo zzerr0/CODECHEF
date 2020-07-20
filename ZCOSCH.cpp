639cdb1d45d606f5f9a1dccccb4af5ab673b3201
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-07-20 20:44:56 +0530

    Create ZCOSCH.cpp

---
+++ ZCOSCH.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int r;
+  cin>>r;
+  
+  if(r>=1 && r<= 50)
+  cout<<"100"<<endl;
+  else 
+  if(r>=51 && r<=100)
+  cout<<"50"<<endl;
+  else 
+  cout<<"0"<<endl;
+  
+  return 0;
+  
+}
