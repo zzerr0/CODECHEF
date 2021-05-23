1e2a7ff827ba612a7ba062eaa4fa381d54215117
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2021-05-23 16:22:14 +0530

    Create pen.cpp

---
+++ pen.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+float a=0, b=0, x=0, y=0;
+cin>>a>>x>>b>>y;
+float pen=0, pencil=0;
+pen = a*x;
+pencil = b*y;
+
+cout<<pen<<endl<<pencil<<endl;
+return 0;
+}
